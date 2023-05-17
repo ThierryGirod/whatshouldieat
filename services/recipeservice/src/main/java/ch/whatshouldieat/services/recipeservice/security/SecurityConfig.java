68339d3da2525aefd5d0f9e64efd69f9fb54fc90
Oliver Faust <oli_fau@hotmail.com>
2023-05-17 11:02:47 +0200

    added token based security to recipe endpoints

--- services/recipeservice/src/main/java/ch/whatshouldieat/services/recipeservice/security/SecurityConfig.java
+++ services/recipeservice/src/main/java/ch/whatshouldieat/services/recipeservice/security/SecurityConfig.java
@@ -1,6 +1,26 @@
 package ch.whatshouldieat.services.recipeservice.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    //TODO
-    
+   
+   @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                .authorizeHttpRequests(authorize -> authorize
+                    //only with valid token
+                    .requestMatchers("/recipe/**")
+                    .authenticated()            
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt
+                );
+            return http.build();
+    }
 }
